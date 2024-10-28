5079394da8203300293f1b4d2e8bf23d9ab5de56
Nikita V <swebs.sh@gmail.com>
2024-10-28 11:59:09 +0500

    Создал модель и миграцию для сущности конференции

---
+++ database/migrations/2024_10_28_064501_create_conferences_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conferences', function (Blueprint $table) {
+            $table->id();
+            $table->text('preview');
+            $table->date('date_start');
+            $table->date('date_end')->nullable(); // nullable;
+            // NAME
+            $table->string('name_ru');
+            $table->string('name_kz');
+            $table->string('name_en');
+            // DESCRIPTION
+            $table->longText('description_ru')->nullable();
+            $table->longText('description_kz')->nullable();
+            $table->longText('description_en')->nullable();
+            // CONTACTS
+            $table->text('contacts_ru')->nullable();
+            $table->text('contacts_kz')->nullable();
+            $table->text('contacts_en')->nullable();
+            // FILES
+            $table->longText('abstracts_file')->nullable();
+            $table->longText('program_file')->nullable();
+            //
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conferences');
+    }
+};
